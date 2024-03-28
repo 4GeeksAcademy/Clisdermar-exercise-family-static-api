ba22eee98814cd47873b970e005c6a8b00e149de
clisdermar <clisdermar@gmail.com>
2024-03-28 05:17:24 +0000

    3er intento

--- src/datastructures.py
+++ src/datastructures.py
@@ -21,9 +21,9 @@ class FamilyStructure:
         # example list of members
         self._members = [ 
               {"id": 1, "first_name": "John", "age": 33, "lucky_numbers": [7, 13, 22]},
-              {"id": 2, "first_name": "Jane", "age": 35, "lucky_numbers": [10, 14, 3]},
-              {"id": 3, "first_name": "Jimmy", "age": 5, "lucky_numbers": [1]}
-           ]
+            {"id": 2, "first_name": "Jane", "age": 35, "lucky_numbers": [10, 14, 3]},
+            {"id": 3, "first_name": "Jimmy", "age": 5, "lucky_numbers": [1]}
+        ]
 
     # read-only: Use this method to generate random members ID's when adding members into the list
     def _generateId(self):
